366e5124aa82a774082612a5244b5a3248a21286
mihainipomici <mihainipomici@gmail.com>
2023-10-05 15:03:29 +0200

    created git repo with cookiecutter

---
+++ docs/index.rst
@@ -0,0 +1,24 @@
+.. msc_thesis_dtu documentation master file, created by
+   sphinx-quickstart.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+msc_thesis_dtu documentation!
+==============================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   getting-started
+   commands
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This research dives into the transportation landscape of Copenhagen, focusing on how various modes like trains, shared cars, and bikes work together. We'll map out the connections between these modes and try to predict their future usage based on current trends. Our approach will combine standard data analysis techniques with machine learning to make these forecasts',
+    author='Mihai Nipomici & Ion Cararus',
+    license='MIT',
+)
